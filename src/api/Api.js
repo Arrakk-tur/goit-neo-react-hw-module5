2f55ed017878dde7bad15d9f0a903243bc44d239
Kostiantyn Skliar <skljar.k@gmail.com>
2025-03-12 16:56:53 +0100

    api

--- src/api/Api.js
+++ src/api/Api.js
@@ -2,28 +2,76 @@ import axios from "axios";
 
 const API_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTJlZjA4NmEzMWVlY2RhZGExODVhYmJkNTY3ZWQzOCIsIm5iZiI6MTc0MTE5MDUzMy41MDEsInN1YiI6IjY3Yzg3NTg1M2RlMzA0MjFiN2MyYWRkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7WuVC2jUp0V7XbZnysA-SR54nsb9VrT4TITKtO-mkLE";
-const BASE_URL = "https://api.themoviedb.org/3/authentication";
+const BASE_URL = "https://api.themoviedb.org/3";
 
-const unsplashAPI = axios.create({
-  baseURL: BASE_URL,
+const options = {
   headers: {
+    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
-});
+};
 
-export const searchPhotos = async (query, page = 1, perPage = 12) => {
+export const fetchTrendingMovies = async () => {
   try {
-    const response = await unsplashAPI.get("/search/photos", {
-      params: {
-        query,
-        page,
-        per_page: perPage,
-      },
-    });
+    const response = await axios.get(
+      `${BASE_URL}/trending/movie/day?language=en-US`,
+      options
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return [];
+  }
+};
+
+export const searchMovies = async (query) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/search/movie?include_adult=false&language=en-US&page=1&query=${query}`,
+      options
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error("Error searching movies:", error);
+    return [];
+  }
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}?language=en-US`,
+      options
+    );
     return response.data;
   } catch (error) {
-    throw new Error(
-      error.response?.data?.errors?.[0] || "Failed to fetch photos"
+    console.error("Error fetching movie details:", error);
+    return null;
+  }
+};
+
+export const fetchMovieCredits = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}/credits?language=en-US`,
+      options
     );
+    return response.data.cast;
+  } catch (error) {
+    console.error("Error fetching movie credits:", error);
+    return [];
+  }
+};
+
+export const fetchMovieReviews = async (movieId) => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/movie/${movieId}/reviews?language=en-US&page=1`,
+      options
+    );
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching movie reviews:", error);
+    return [];
   }
 };
